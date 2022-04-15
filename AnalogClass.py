f223aa6dc357bb2f07c99cb1e2271fe3dad8d431
R4ENG <rodrigo_alcaraz_f@hotmail.com>
2022-04-15 08:30:54 -0400

    Hola tambien

---
+++ AnalogClass.py
@@ -0,0 +1,42 @@
+class Vehiculos():
+    
+    def __init__(self, marca, modelo):
+        
+        self.marca=marca
+        self.modelo=modelo
+        self.enmarcha=False
+        self.acelera=False
+        self.frena=False
+        
+    def arrancar(self):
+        
+        self.enmarcha=True
+        
+    def acelerar(self):
+        
+        self.acelera=True
+        
+    def frenar(self):
+        
+        self.frena=True
+
+    def estado(self):
+        
+        print('Marca:', self.marca, '\nModelo:', self.modelo)
+        
+ 
+class Moto(Vehiculos):
+    pass
+
+miMoto=Moto('Honda', 'CBR')
+
+miMoto.estado()
+
+miVehiculo=Vehiculos('Toyota','HiLux')
+
+miVehiculo.estado()
+
+print("hola")
+
+
+print("Saludos a todos, porque si")
